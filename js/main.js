caee89611c627b69181e7f5f1c9b5ca2c9ea0a63
osnpro <olgaosn@walla.com>
2025-09-25 16:52:38 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,30 +1,59 @@
 'use strict'
 // cells tipe
-const MINE = '@'
+const MINE = '💣'
 const EMPTY = ' '
 const HIDDEN = '?'
-
+const BOARD_SIZE = 4
 
 var gBoard
 
-gBoard = buildBoard()
+function restartGame() {
+    gBoard = buildBoard()
+    renderBoard(gBoard, '#board')
+}
+
+
+
+
 
 function buildBoard() {
     var board = []
 
 
-    for (var i = 0; i < 4; i++) {
+    for (var i = 0; i < BOARD_SIZE; i++) {
         board[i] = []
 
 
-        for (var j = 0; j < 4; j++) {
+        for (var j = 0; j < BOARD_SIZE; j++) {
             board[i][j] = HIDDEN
-            if (i === 2 && j === 2) board[i][j] = MINE
-            if (i === 3 && j === 3) board[i][j] = MINE
-
+            
         }
     }
+    board[2][2] = MINE
+    board[3][3] = MINE
     console.table(board)
     return board
 
 }
+
+function renderBoard(board, selector) {
+
+    var strHTML = '<table><tbody>'
+    for (var i = 0; i < board.length; i++) {
+
+        strHTML += '<tr>'
+        for (var j = 0; j < board[0].length; j++) {
+
+            const cell = board[i][j]
+            const className = `cell cell-${i}-${j}`
+
+            strHTML += `<td class="${className}">${cell}</td>`
+        }
+        strHTML += '</tr>'
+    }
+    strHTML += '</tbody></table>'
+
+    const elContainer = document.querySelector(selector)
+    elContainer.innerHTML = strHTML
+}
+restartGame()
